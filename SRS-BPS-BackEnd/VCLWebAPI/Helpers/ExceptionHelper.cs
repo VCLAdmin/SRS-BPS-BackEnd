bb40a3cdf6f426a6ccd0566c32c18077c9d31af3
HipMorillon <102514876+HipMorillon@users.noreply.github.com>
2022-05-24 11:20:45 -0400

    Feat : setup the exception helper to log exceptions

---
+++ SRS-BPS-BackEnd/VCLWebAPI/Helpers/ExceptionHelper.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using VCLWebAPI.Models;
+using log4net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using VCLWebAPI.Exceptions;
+using VCLWebAPI.Utils;
+
+namespace Services.Helpers
+{
+    public static class ExceptionHelper
+    {
+        static readonly ILog log = log4net.LogManager.GetLogger(nameof(ExceptionHelper));
+
+        /// <summary>
+        /// Filters the exception using our custom exception types.
+        /// </summary>
+        /// <param name = "context" > The context in wich the exception was thrown.</param>
+        /// <param name = "cancellationToken" > The cancellation token.</param>
+        /// <returns></returns>
+        public static void FilterException(ExceptionContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            HttpStatusCode status = HttpStatusCode.InternalServerError;
+            var customException = context.Exception as CustomException;
+            string message;
+
+            if (customException == null)
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.InternalServerError;
+            }
+
+            if (customException is InvalidModelException)
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.BadRequest;
+            }
+            else if (customException is NotFoundException)
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.NotFound;
+            }
+            else if (customException is UnauthorizedException)
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.Unauthorized;
+            }
+            else if (customException is ForbiddenException)
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.Forbidden;
+            }
+            else if (customException is ConflictException)
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.Conflict;
+            }
+            else
+            {
+                message = context.Exception.ToString();
+                status = HttpStatusCode.InternalServerError;
+            }
+
+            context.ExceptionHandled = true;
+
+            HttpResponse response = context.HttpContext.Response;
+            response.StatusCode = (int)status;
+            response.ContentType = "application/json";
+            var err = message + " " + context.Exception.StackTrace;
+
+            // we log the exception in the log file
+
+            try
+            {
+                if(status != HttpStatusCode.Unauthorized)
+                {
+                    // Unauthorized exceptions should not be logged
+                    ExceptionHelper.LogException(context, status, message);
+                }
+            }
+            catch(Exception e)
+            {
+                // we don't do anything, just continue with the request
+            }
+
+            response.WriteAsync(err, cancellationToken: cancellationToken);
+        }
+
+        public static void LogException(ExceptionContext context, HttpStatusCode status, string message)
+        {
+            ControllerActionDescriptor actionDescriptor = (ControllerActionDescriptor)context.ActionDescriptor;
+
+            var userClaims = ApiUtil.ExtractUserClaims(context.HttpContext.User.Claims);
+
+            var exceptionLog = new ExceptionLog
+            {
+                Action = actionDescriptor.ActionName,
+                Controller = actionDescriptor.ControllerName,
+                DateTime = DateTime.UtcNow,
+                ErrorCode = status.ToString(),
+                Exception = context.Exception.GetType().Name,
+                ExceptionDetail = message,
+                ExceptionLogExternalId = Guid.NewGuid(),
+                InnerException = context.Exception.InnerException != null ? context.Exception.InnerException.Message : null,
+                RequestUrl = context.HttpContext.Request.Path.Value,
+                User = userClaims.Email,
+                UserExternalId = userClaims.UserId,
+                URI = context.HttpContext.Request.Path.Value,
+                RequestUrlReferrer = context.HttpContext.Request.Headers["Origin"],
+                RequestUserAgent = context.HttpContext.Request.Headers["User-Agent"]
+            };
+
+            var msg = exceptionLog.ErrorCode + " ; " + exceptionLog.DateTime + " ; " + exceptionLog.Controller + " ; " + exceptionLog.Action + " ; " +
+                exceptionLog + " ; " + exceptionLog.UserExternalId + " ; " + exceptionLog.URI + " ; " + exceptionLog.ExceptionDetail + " ; " +
+                exceptionLog.RequestUrl + " ; " + exceptionLog.RequestUrlReferrer + " ; " + exceptionLog.RequestUserAgent;
+
+            LogToTextFile(msg);
+        }
+
+        public static void LogToTextFile(string msg)
+        {
+            try
+            {
+                log.Info(msg); //write to a file using log4net
+
+                //string filepath = Globals.AppPhysicalPath + @"\logs\";  //Text File Path
+
+                //if (!Directory.Exists(filepath))
+                //{
+                //    Directory.CreateDirectory(filepath);
+
+                //}
+                //filepath = filepath + DateTime.Today.ToString("MM.dd.yyyy") + "_Application_Error_Log.txt";   //Text File Name
+                //if (!File.Exists(filepath))
+                //{
+                //    File.Create(filepath).Dispose();
+
+                //}
+                //using (StreamWriter sw = File.AppendText(filepath))
+                //{
+                //    //sw.WriteLine(GetLog());
+                //    sw.WriteLine(DateTime.Now.ToLongTimeString() + "---------------------------------------------------------------------");
+                //    sw.WriteLine(msg);
+                //    sw.Flush();
+                //    sw.Close();
+                //}
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+        }
+    }
+}
---
+++ SRS-BPS-BackEnd/VCLWebAPI/Models/ExceptionLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VCLWebAPI.Models
+{
+    public class ExceptionLog
+    {
+        public long ExceptionLogId { get; set; }
+        public System.Guid ExceptionLogExternalId { get; set; }
+        public Nullable<System.DateTime> DateTime { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string ErrorCode { get; set; }
+        public string Exception { get; set; }
+        public string URI { get; set; }
+        public string ExceptionDetail { get; set; }
+        public string InnerException { get; set; }
+        public string RequestUrl { get; set; }
+        public string RequestUrlReferrer { get; set; }
+        public string RequestUserAgent { get; set; }
+    }
+}
