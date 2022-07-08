4c0c9ede2b6ee364bc8c0aa6982a5d450a3dd8b0
Hippolyte Morillon <HMorillon@schuco-usa.com>
2022-07-08 14:41:14 -0400

    Revert "Revert "Merge branch 'SRS-BPSS-Backend-SSO' into SRS-BPS-BackEnd_BPS-Production""
    This reverts commit 06ac10c131e9fa78b19c097e8498251cb7a04050.

--- VCLWebAPI/Controllers/AccountController.cs
+++ VCLWebAPI/Controllers/AccountController.cs
@@ -19,6 +19,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using VCLWebAPI.Models.Edmx;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace VCLWebAPI.Controllers
 {
@@ -533,6 +534,60 @@ namespace VCLWebAPI.Controllers
             //}
 
             return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task PasswordMigrationforBPSSSO()
+        {
+            string newPassword = "4nEX25CjFvQw8bOjB59Y3xuA9Tnv5j!";
+            await _accountService.PasswordMigrationforBPSSSO(newPassword);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("LoginToken")]
+        public async Task<object> Get(string token = "")
+        {
+            var tokenHandler = new Services.TokenHandler();
+
+            if (token.StartsWith("token="))
+            {
+                token = token.Remove(0, 6);
+            }
+
+            var res = tokenHandler.DecodeHexString(token);
+            DecryptedToken decryptedToken = JsonConvert.DeserializeObject<DecryptedToken>(res);
+
+            var response = await tokenHandler.RequestValidation(decryptedToken.loginName, decryptedToken.securitytoken);
+
+            // if user exists
+            //ApplicationUser existingUser = await UserManager.FindByEmailAsync(decryptedToken.email);
+            User existingUser = _accountService.GetUserByEmail(decryptedToken.email);
+
+            if (existingUser == null)
+            {
+                // if user doesn't exist
+                var user = new ApplicationUser() { UserName = decryptedToken.email, Email = decryptedToken.email };
+                var defaultPassword = "4nEX25CjFvQw8bOjB59Y3xuA9Tnv5j!";
+                IdentityResult result = await _userManager.CreateAsync(user, defaultPassword);
+
+                // Create a user in the table
+                var name = decryptedToken.givenName.Split(' ');
+                _accountService.CreateUser(decryptedToken.givenName, decryptedToken.name, decryptedToken.email);
+            }
+
+            if (((int)response) == 200)
+            {
+                return decryptedToken;
+            }
+            else
+            {
+                DecryptedToken returnToken = new DecryptedToken();
+                returnToken.email = "Validationfailed";
+                //return "Validation failed";
+                return returnToken;
+            }
         }
 
         /// <summary>
---
+++ VCLWebAPI/Models/Account/DecryptedToken.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VCLWebAPI.Models.Account
+{
+    public class DecryptedToken
+    {
+        [JsonProperty(PropertyName = "security-token")]
+        public string securitytoken { get; set; }
+
+        public string time { get; set; }
+        public string name { get; set; }
+        public string givenName { get; set; }
+        public string email { get; set; }
+        public string loginName { get; set; }
+        public string userType { get; set; }
+        public List<string> additionalRoles { get; set; }
+    }
+}
